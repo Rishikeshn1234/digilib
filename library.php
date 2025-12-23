eb808ce1f17285cf269ce0b298f32a80464193c4
Rishikeshn123 <jadhavrishikeshn@gmail.com>
2025-12-23 20:22:20 +0530

    creation of class and methods

---
+++ library.php
@@ -0,0 +1,91 @@
+<?php
+class Library
+{
+    private $conn;
+
+    //Function to connect to the database
+    public function connect_db($server,$root,$password,$database)
+    {
+        $this->conn=new mysqli($server,$root,$password,$database);
+        if($this->conn->connect_error)
+        {
+            return false;
+            die('Unable to connect to the database');
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    //Function to close the connection
+    public function close_db()
+    {
+        if($this->conn)
+        {
+            $this->conn->close();
+        }
+    }
+
+    //Function to look after the registration
+    public function register()
+    {
+        $uname=$_POST['uname'];
+        $password=$_POST['password'];
+        $cpassword=$_POST['cpassword'];
+        $email=$_POST['email'];
+
+        $uname=filter_var($uname,FILTER_SANITIZE_SPECIAL_CHARS);
+        $password=filter_var($password,FILTER_SANITIZE_SPECIAL_CHARS);
+        $cpassword=filter_var($cpassword,FILTER_SANITIZE_SPECIAL_CHARS);
+        $email=filter_var($email,FILTER_SANITIZE_EMAIL);
+
+        if($password!==$cpassword)
+        {
+            return false;
+        }
+        else
+        {
+            $sql="SELECT * FROM users WHERE uname='$uname'";
+            $result=$this->conn->query($sql);
+            if($result->num_rows>0)
+            {
+                return false;
+            }
+            else
+            {
+                $sql="INSERT INTO users(uname,password,email,c_time) VALUES('$uname','$password','$email',NOW())";
+                if($this->conn->query($sql))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+
+    //Function to look after login
+    public function login()
+    {
+        $uname=$_POST['uname'];
+        $password=$_POST['password'];
+        
+        $uname=filter_var($uname,FILTER_SANITIZE_SPECIAL_CHARS);
+        $password=filter_var($password,FILTER_SANITIZE_SPECIAL_CHARS);
+        
+        $sql="SELECT * FROM users WHERE uname='$uname' AND password='$password'";
+        $result=$this->conn->query($sql);
+        if($result->num_rows>0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+?>
